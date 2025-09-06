e02ebc648f6fe1ccf6b871637d8e2a7512899a31
Guilherme Ramos <127454792+ramosgk@users.noreply.github.com>
2025-09-05 21:02:55 -0300

    Update listar-projeto.php

--- projeto/views/listar-projeto.php
+++ projeto/views/listar-projeto.php
@@ -2,11 +2,15 @@
     <div class="card-body">
         <h5 class="card-title">Listar Projetos</h5>
         <?php
+        // Query para selecionar todos os projetos da tabela 'projeto'.
         $sql = "SELECT * FROM projeto";
         $res = $conn->query($sql);
         $qtd = $res->num_rows;
+
+        // Condição que verifica se há projetos no banco de dados.
         if($qtd > 0){
             print "<div class='table-responsive'>";
+            // A tabela usa classes do Bootstrap para um visual corporativo.
             print "<table class='table table-hover table-striped table-bordered'>"; 
             print "<thead>";
             print "<tr class='table-dark'>";
@@ -22,6 +26,7 @@
             print "</tr>";
             print "</thead>";
             print "<tbody>";
+            // Loop para percorrer cada projeto encontrado no banco de dados.
             while($row = $res->fetch_object()){
                 print "<tr>";
                 print "<td>" .$row->id."</td>";
@@ -31,6 +36,7 @@
                 print "<td>" .$row->cidade."</td>";
                 print "<td>" .$row->tipo."</td>";
                 print "<td>" .$row->status."</td>";
+                // Formata a data para o padrão brasileiro.
                 print "<td>" .date('d/m/Y', strtotime($row->data_inicio))."</td>";
                 print "<td>
                 <a href='?page=editar&id=".$row->id."' class='btn btn-dark btn-sm me-1'>Editar</a>
@@ -43,6 +49,7 @@
             print "</table>";
             print "</div>";
         } else {
+            // Mensagem de alerta se nenhum projeto for encontrado.
             print "<p class='alert alert-warning'>Nenhum projeto encontrado!</p>";
         }
         ?>
